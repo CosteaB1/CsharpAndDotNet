882d8b67ff49566d54169fd87ef52a52fb387e68
Constantin Bivol <constantin.bivol@amdaris.com>
2021-06-24 14:55:43 +0300

    Create ConditionalConstructs.cs

---
+++ BasicsOfProgrammingCsharp/ConditionalConstructs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicsOfProgrammingCsharp
+{
+    public class ConditionalConstructs
+    {
+        public void IfElseStatement(int firstNumber, int secondNumber)
+        {
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine($"Число {firstNumber} больше числа {secondNumber}");
+            }
+
+        }
+    }
+}
